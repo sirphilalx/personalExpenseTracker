3d6d2cce1639788d0ca12c0280fae749eff4e151
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-01-19 22:39:34 +0100

    created the income schema in the models

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { db } = require("./db/db");
+const { readdirSync } = require("fs");
 
 const app = express();
 
@@ -9,6 +10,11 @@ require("dotenv").config();
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+//routes
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
 const PORT = process.env.PORT;
 
