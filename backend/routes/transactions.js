56ff5154af53e82957d841a75ddc0e0524cc31e2
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-01-20 11:21:12 +0100

    connection to database

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,7 @@
+const { addIncome } = require("../controllers/income");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("Welcome to the homepage");
-});
+router.post("/add-income", addIncome);
 
 module.exports = router;
